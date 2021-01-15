c54cb7cb45811f53cb24c788d6e8365f04a4e87a
tytydraco <tylernij@gmail.com>
2021-01-15 13:52:14 -0800

    Fullscreen verify screen
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/sated/views/VerifyActivity.kt
+++ app/src/main/java/com/draco/sated/views/VerifyActivity.kt
@@ -2,7 +2,11 @@ package com.draco.sated.views
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.animation.LinearInterpolator
 import android.widget.CheckBox
 import android.widget.ProgressBar
@@ -25,6 +29,11 @@ class VerifyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify)
+
+        window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+            view.post { immersive() }
+            return@setOnApplyWindowInsetsListener windowInsets
+        }
 
         verifyBox1 = findViewById(R.id.verify_1)
         verifyBox2 = findViewById(R.id.verify_2)
@@ -64,6 +73,23 @@ class VerifyActivity : AppCompatActivity() {
                 if (verifySum == 4)
                     finish()
             }
+        }
+    }
+
+    private fun immersive() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            with (window.insetsController!!) {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN
         }
     }
 
