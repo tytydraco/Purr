d8c260b1341053c3ea659489d7ae2d7f72963b0b
tytydraco <tylernij@gmail.com>
2021-01-15 14:33:42 -0800

    Add settings functionality
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/sated/fragments/SettingsPreferenceFragment.kt
+++ app/src/main/java/com/draco/sated/fragments/SettingsPreferenceFragment.kt
@@ -1,11 +1,40 @@
 package com.draco.sated.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.draco.sated.R
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.android.material.snackbar.Snackbar
 
 class SettingsPreferenceFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        when (preference.key) {
+            getString(R.string.pref_developer_key) -> openURL(getString(R.string.developer_url))
+            getString(R.string.pref_source_key) -> openURL(getString(R.string.source_url))
+            getString(R.string.pref_contact_key) -> openURL(getString(R.string.contact_url))
+            getString(R.string.pref_licenses_key) -> {
+                val intent = Intent(requireContext(), OssLicensesMenuActivity::class.java)
+                startActivity(intent)
+            }
+            else -> return super.onPreferenceTreeClick(preference)
+        }
+        return true
+    }
+
+    private fun openURL(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Snackbar.make(requireView(), getString(R.string.snackbar_intent_failed), Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
