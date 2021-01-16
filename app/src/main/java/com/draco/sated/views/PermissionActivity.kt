8366ddbfaddc2cc1ee0e5ef3a7aa7205e8d47cb0
tytydraco <tylernij@gmail.com>
2021-01-15 16:34:14 -0800

    Add help button
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/sated/views/PermissionActivity.kt
+++ app/src/main/java/com/draco/sated/views/PermissionActivity.kt
@@ -4,7 +4,10 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Html
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +18,7 @@ import com.google.android.material.snackbar.Snackbar
 class PermissionActivity : AppCompatActivity() {
     private val viewModel: PermissionActivityViewModel by viewModels()
 
+    private lateinit var learnMore: ImageButton
     private lateinit var command: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +30,19 @@ class PermissionActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_permission)
+        learnMore = findViewById(R.id.learn_more)
         command = findViewById(R.id.command)
+
+        learnMore.setOnClickListener {
+            val url = getString(R.string.learn_more_url)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            try {
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Snackbar.make(learnMore, getString(R.string.snackbar_intent_failed), Snackbar.LENGTH_SHORT).show()
+            }
+        }
 
         command.setOnClickListener {
             val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
